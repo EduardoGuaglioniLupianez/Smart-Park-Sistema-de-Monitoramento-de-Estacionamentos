60d6d43ae39204fa4438200347057337f5ac9030
Victor de Sousa Oliveira <victor.soliveira@sptech.school>
2026-03-09 11:02:31 -0300

    Adicionando Fahrenheit ao código do Arduíno

--- Código_Arduino_Teste/codigo_arduino.ino
+++ Código_Arduino_Teste/codigo_arduino.ino
@@ -1,5 +1,6 @@
 const int PINO_SENSOR_TEMPERATURA = A0;
 float temperaturaCelsius;
+float temperaturaFahrenheit;
 
 void setup() {
   Serial.begin(9600);
@@ -7,11 +8,17 @@ void setup() {
 
 void loop() {
   int valorLeitura = analogRead(PINO_SENSOR_TEMPERATURA);
-  temperaturaCelsius = (valorLeitura * 5.0 / 1023.0) / 0.01;
-
+  temperaturaCelsius = (valorLeitura * 0.48828125) - 50;
+  temperaturaFahrenheit = (temperaturaCelsius * 1.8) + 32;
+  
+  Serial.println("--------------");
   Serial.print("Temperatura: ");
   Serial.print(temperaturaCelsius);
   Serial.println(" C");
+  
+  Serial.print("Temperatura: ");
+  Serial.print(temperaturaFahrenheit);
+  Serial.println(" F");
   
   delay(2000);
 }
